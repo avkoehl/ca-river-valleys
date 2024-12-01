d0506fbc3fa75ae4a25abdecce6dbee78c720cb3
Arthur Koehl <avkoehl@ucdavis.edu>
2024-11-30 22:44:17 -0800

    will cause merge conflict but need these changes

--- src/download_huc.py
+++ src/download_huc.py
@@ -14,15 +14,40 @@ def get_dem_and_flowlines(hucid, layer):
     boundary = wbd.byids(layer, hucid)
     boundary_reprojected = boundary.to_crs(3310)
 
-    dem = py3dep.static_3dep_dem(boundary.geometry.iloc[0], resolution=10, crs=4326)
-    dem = dem.rio.reproject(3310)
+    try:
+        dem = py3dep.static_3dep_dem(boundary.geometry.iloc[0], resolution=10, crs=4326)
+    except:
+        dem = retry_on_smaller(boundary)
 
+    dem = dem.rio.reproject("EPSG:3310", 
+            resampling=rasterio.enums.Resampling.bilinear)
     nhd_mr = NHD("flowline_mr")
     flowlines_mr = nhd_mr.bygeom(boundary.geometry.iloc[0].bounds)
     flowlines_mr = flowlines_mr.to_crs(3310)
     flowlines_mr = flowlines_mr.clip(boundary_reprojected.geometry.iloc[0])
 
     return dem, flowlines_mr
+
+def retry_on_smaller(boundary):
+    bbox = geom.bounds
+    bbox['mid_x'] = (bbox['minx'] + bbox['maxx']) / 2
+    bbox['mid_y'] = (bbox['miny'] + bbox['maxy']) / 2
+    
+    # Create four smaller bounding boxes
+    split_bboxes = pd.DataFrame([
+        {'minx': bbox.iloc[0]['minx'], 'miny': bbox.iloc[0]['miny'], 'maxx': bbox.iloc[0]['mid_x'], 'maxy': bbox.iloc[0]['mid_y']},  # Bottom-left
+        {'minx': bbox.iloc[0]['mid_x'], 'miny': bbox.iloc[0]['miny'], 'maxx': bbox.iloc[0]['maxx'], 'maxy': bbox.iloc[0]['mid_y']},  # Bottom-right
+        {'minx': bbox.iloc[0]['minx'], 'miny': bbox.iloc[0]['mid_y'], 'maxx': bbox.iloc[0]['mid_x'], 'maxy': bbox.iloc[0]['maxy']},  # Top-left
+        {'minx': bbox.iloc[0]['mid_x'], 'miny': bbox.iloc[0]['mid_y'], 'maxx': bbox.iloc[0]['maxx'], 'maxy': bbox.iloc[0]['maxy']}   # Top-right
+    ])
+    
+    dems = []
+    for ind,box in split_bboxes.iterrows():
+        dems.append(py3dep.get_dem(tuple(box), crs="EPSG:4326", resolution=10))
+
+    mosaic = merge_arrays(dems)
+    clipped = mosaic.rio.clip(boundary)
+    return clipped
 
 def main():
     hucid = sys.argv[1]
