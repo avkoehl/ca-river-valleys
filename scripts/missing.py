d6582f38a39edca06a19060604e08b2812c2b63c
Arthur Koehl <avkoehl@ucdavis.edu>
2025-01-03 18:24:18 -0800

    cleanup

---
+++ scripts/missing.py
@@ -0,0 +1,27 @@
+# load list of all the huc10s, compare with list of all the completed
+
+from glob import glob
+
+import pandas as pd
+
+
+floors_dir = "../../20241231-CA_floors/"
+manifest_file = "../data/huc_manifest/huc_manifest.csv"
+
+hucs = pd.read_csv(manifest_file)
+
+floors_files = glob(f"{floors_dir}*-floors.tif")
+completed = []
+for f in floors_files:
+    huc10 = f.split('/')[-1].split('-')[0]
+    completed.append(huc10)
+
+# possible
+huc10s = hucs.loc[hucs['level'] == 'huc10']
+huc10s['hucid'] = huc10s['hucid'].astype('str')
+huc10s = huc10s.loc[huc10s['hucid'].str.startswith('18')]
+
+# missing
+missing = set(huc10s['hucid']) - set(completed)
+
+
---
+++ scripts/mosaic_huc6.py
@@ -0,0 +1,3 @@
+# load in all the tif filenames
+# for each huc 6, load all the files and moasaic and clip to california land polygon
+# save
